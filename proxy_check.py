20fa07e032544c1c0f94952836e1b9be85e3861e
reagans <reagans@amazon.com>
2019-12-02 16:31:02 -0800

    Reformatted with BLACK

--- proxy_check.py
+++ proxy_check.py
@@ -10,18 +10,16 @@ good_list = []
 
 
 def get_args(argv=None):
-    parser = argparse.ArgumentParser(prog='ProxyCheck',
-                                     usage='%(prog)s [-h] -i PROXY_FILE [-o OUT_FILE] -t THREADS',
-                                     description='Test list of proxies for reachability and function')
-    parser.add_argument('-i', '--proxy_file',
-                        required=True,
-                        help='Proxy File Input')
-    parser.add_argument('-o', '--output_file',
-                        help='Output File')
-    parser.add_argument('-t', '--threads',
-                        required=True,
-                        type=int,
-                        help='Number of threads to run on')
+    parser = argparse.ArgumentParser(
+        prog="ProxyCheck",
+        usage="%(prog)s [-h] -i PROXY_FILE [-o OUT_FILE] -t THREADS",
+        description="Test list of proxies for response and function",
+    )
+    parser.add_argument("-i", "--proxy_file", required=True, help="Proxy File Input")
+    parser.add_argument("-o", "--output_file", help="Output File")
+    parser.add_argument(
+        "-t", "--threads", required=True, type=int, help="Number of threads to run on"
+    )
     return parser.parse_args(argv)
 
 
@@ -30,24 +28,28 @@ def verify_proxy(proxy_list, thread_number):
     for item in proxy_list:
         try:
             proxy_dict = {"http": item, "https": item}
-            r = requests.get("http://ipinfo.io/json", proxies=proxy_dict, timeout=timeout)
+            r = requests.get(
+                "http://ipinfo.io/json", proxies=proxy_dict, timeout=timeout
+            )
             response = r.json()
-            ip = response['ip']
-            print(f'[Thread: {thread_number}] Current IP: {ip}')
-            print(f'[Thread: {thread_number}] Proxy Active: {item}')
-            print(f'[Thread: {thread_number}] Proxy Works: {True if ip == item.split(":")[0] else False}')
+            ip = response["ip"]
+            print(f"[Thread: {thread_number}] Current IP: {ip}")
+            print(f"[Thread: {thread_number}] Proxy Active: {item}")
+            print(
+                f'[Thread: {thread_number}] Proxy Works: {True if ip == item.split(":")[0] else False}'
+            )
             working_list.append(item)
         except Exception as e:
-            print(f'[Thread: {thread_number}] Proxy Failed: {item}')
-            print(f'[Thread: {thread_number}] Proxy Failed: {e}')
+            print(f"[Thread: {thread_number}] Proxy Failed: {item}")
+            print(f"[Thread: {thread_number}] Proxy Failed: {e}")
 
-    print(f'[Thread: {thread_number}] Working Proxies: {len(working_list)}')
+    print(f"[Thread: {thread_number}] Working Proxies: {len(working_list)}")
     good_list.extend(working_list)
 
 
 def get_proxies(file):
     proxy_list = []
-    for item in open(file, 'r+').readlines():
+    for item in open(file, "r+").readlines():
         proxy_list.append(item.strip())
     return proxy_list
 
@@ -55,10 +57,12 @@ def get_proxies(file):
 def setup(number_threads):
     thread_count = float(number_threads)
     proxy_list = get_proxies(args.proxy_file)
-    amount = int(math.ceil(len(proxy_list)/thread_count))
-    proxy_lists = [proxy_list[x:x + amount] for x in range(0, len(proxy_list), amount)]
+    amount = int(math.ceil(len(proxy_list) / thread_count))
+    proxy_lists = [
+        proxy_list[x : x + amount] for x in range(0, len(proxy_list), amount)
+    ]
     if len(proxy_list) % thread_count > 0.0:
-        proxy_lists[len(proxy_lists) - 1].append(proxy_list[len(proxy_list)-1])
+        proxy_lists[len(proxy_lists) - 1].append(proxy_list[len(proxy_list) - 1])
     return proxy_lists
 
 
@@ -75,17 +79,17 @@ def main(threads):
     for x in thread_list:
         x.join()
 
-    print(f'Working Proxies: {good_list}')
+    print(f"Working Proxies: {good_list}")
 
-    with open('good_proxies.txt', 'w') as f:
+    with open("good_proxies.txt", "w") as f:
         for i in good_list:
-            f.write(i + '\n')
+            f.write(i + "\n")
 
     stop_time = time.time()
-    print(f'Completed in {stop_time - start_time} seconds.')
+    print(f"Completed in {stop_time - start_time} seconds.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     argvals = None
     args = get_args(argvals)
     main(args.threads)
